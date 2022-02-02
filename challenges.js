a68d776766e925e9bfa8ea7c0616d725c0495c92
Alex Platt <alja.platt@gmail.com>
2022-02-02 08:30:48 +0000

    adding arr items to ul

---
+++ challenges.js
@@ -0,0 +1,25 @@
+const firstName = "Alex";
+const lastName = "Platt";
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let yaname = "Linda";
+
+let hey = "hey";
+
+const hi = () => {
+  console.log(hey + " " + yaname);
+};
+
+let myPoints = 3;
+
+function add3Points() {
+  myPoints += 3;
+  console.log(myPoints);
+}
+
+function remove1Point() {
+  myPoints -= 1;
+  console.log(myPoints);
+}
